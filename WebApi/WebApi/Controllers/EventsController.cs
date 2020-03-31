cc13e3d3ed34bfcf7b8b7f3a36890ae53d65550a
Johan Strandberg <johan.strandberg@xlent.se>
2020-03-31 09:07:26 +0200

    Initial commit

---
+++ WebApi/WebApi/Controllers/EventsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventsController : ControllerBase
+    {
+        private readonly ILogger<EventsController> _logger;
+
+        public EventsController(ILogger<EventsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public void Post(Event theEvent)
+        {
+
+        }
+    }
+}
